0b5b29118a8501ca318a83a8bfac708ac3567b3d
AJ <ajit.jassal@ucalgary.ca>
2021-09-08 22:43:49 -0600

    Uploaded first programming exercise

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author User
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        
+        System.out.println("Did you know that in 1440 (or therebouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.println("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.println("We are like a modern Gutenberg,");
+        System.out.println("printing vast amounts to the waiting console with ease.");
+        
+        System.out.print("Did you know that in 1440 (or therebouts),");
+        System.out.print("Johannes Gutenberg invented the printing press?\n");
+        System.out.print("He started out as a goldsmith!\n");
+        System.out.print("His invention made it easy to print and");
+        System.out.print("distribute books to anyone who wanted one.\n");
+        System.out.print("We are like a modern Gutenberg, ");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
+    
+}
